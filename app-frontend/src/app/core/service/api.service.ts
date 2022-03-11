b42e95f1c92dd5cea34188a03a390c9eed86675f
mehmoodulhaq <mehmood.haq@mercurialminds.com>
2022-03-11 12:56:06 +0500

    API's created in service to consume

--- app-frontend/src/app/core/service/api.service.ts
+++ app-frontend/src/app/core/service/api.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  url = 'https://infra.devskills.app/api/transaction-management';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addTransactions(data: object) {
+    return this.http.post(`${this.url}/transactions`, data);
+  }
+
+  getTransactions() {
+    return this.http.get(`${this.url}/transactions`);
+  }
+
+  getAccountById(account_id: string) {
+    return this.http.get(`${this.url}/accounts/${account_id}`);
+  }
 }

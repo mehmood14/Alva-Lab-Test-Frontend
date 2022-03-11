dabc6fd0f22b688fccdf359d12f08ecc77a3df9d
mehmoodulhaq <mehmood.haq@mercurialminds.com>
2022-03-11 13:14:16 +0500

    overall transactions added

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlertModule } from 'ngx-alerts';
 import { HttpClientModule } from '@angular/common/http';
+import { ComponentsModule } from './components/components.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ComponentsModule,
     AlertModule.forRoot({ timeout: 6000 }),
   ],
   providers: [],
--- app-frontend/src/app/components/transactions/transactions.component.ts
+++ app-frontend/src/app/components/transactions/transactions.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { AlertService } from 'ngx-alerts';
+import { ApiService } from 'src/app/core/service/api.service';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.css']
+  styleUrls: ['./transactions.component.css'],
 })
 export class TransactionsComponent implements OnInit {
+  transactionForm!: FormGroup;
 
-  constructor() { }
+  amount: FormControl = new FormControl('', [Validators.required]);
+  account_id: FormControl = new FormControl('', [Validators.required]);
+  transactions_data = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private allService: ApiService
+  ) {}
 
   ngOnInit(): void {
+    this.form();
+    this.overAllTransactions();
   }
 
+  form() {
+    this.transactionForm = this.formBuilder.group({
+      amount: this.amount,
+      account_id: this.account_id,
+    });
+  }
+
+  overAllTransactions() {
+    this.allService.getTransactions().subscribe(
+      (res: any) => {
+        if (res.success == false) {
+          this.alertService.warning('Something went wrong');
+        } else {
+          this.transactions_data = res;
+        }
+      },
+      (error) => {
+        this.alertService.danger(error);
+      }
+    );
+  }
+
+  onSubmit() {
+    this.allService.addTransactions(this.transactionForm.value).subscribe(
+      (res: any) => {
+        if (res.success == false) {
+          this.alertService.warning('Something went wrong');
+        } else {
+          this.alertService.success('Transaction completed successfully.');
+
+          this.transactionForm.reset();
+        }
+      },
+      (error) => {
+        this.alertService.danger(error);
+      }
+    );
+  }
+
+  clearForm() {}
 }

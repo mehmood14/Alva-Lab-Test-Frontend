6255377c83bba0c5120558d31b4ea59e065705e0
mehmoodulhaq <mehmood.haq@mercurialminds.com>
2022-03-11 16:08:47 +0500

    get account by id API implemented

--- app-express/src/api/index.js
+++ app-express/src/api/index.js
@@ -7,6 +7,23 @@ router.get("/ping", (req, res) => {
 
 router.post("/transactions", (req, res) => {});
 
-router.get("/accounts/:account_id", (req, res) => {});
+router.get("/accounts/:account_id", (req, res) => {
+  try {
+    Account.findOne({ account_id: req.params.account_id }, (error, data) => {
+      if (error) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid data.",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        data: data,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 module.exports = router;

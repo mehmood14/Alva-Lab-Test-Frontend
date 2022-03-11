dad2fd97c2ab5bd93b500256af1c4caa7f743795
mehmoodulhaq <mehmood.haq@mercurialminds.com>
2022-03-11 16:50:07 +0500

    add new transaction api completed

--- app-express/src/api/index.js
+++ app-express/src/api/index.js
@@ -1,21 +1,95 @@
 const express = require("express");
 const router = express.Router();
+let TransactionRequest = require("../models/transaction-request");
+let Account = require("../models/account");
+let Transaction = require("../models/transaction");
 
 router.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-router.post("/transactions", (req, res) => {});
+router.post("/transactions", (req, res) => {
+  try {
+    let { account_id, amount } = req.body;
+
+    let positiveAmount;
+    amount > 0 ? (positiveAmount = 1) : (positiveAmount = -1);
+
+    //add a transaction
+    let transactionRequest = new TransactionRequest();
+    transactionRequest.account_id = account_id;
+    transactionRequest.amount = amount * positiveAmount;
+
+    transactionRequest.save((error, result) => {
+      errorHandling(error, result);
+
+      //find account if exist in account schema
+      Account.find({ account_id: account_id }, (error, data2) => {
+        errorHandling(error, data2);
+
+        //if account doesn't exist add new account in account schema and set balance as amount
+        if (data2.length == 0) {
+          let account = new Account();
+          account.account_id = account_id;
+          account.balance = amount * positiveAmount;
+
+          account.save((error, result) => {
+            errorHandling(error, result);
+          });
+        } else {
+          //update balance depending on if amount is withdrawn or deposited
+          Account.findOneAndUpdate(
+            { account_id: account_id },
+            {
+              balance:
+                amount > 0
+                  ? data2[0].balance + amount * positiveAmount
+                  : data2[0].balance - amount * positiveAmount,
+            },
+            (error, data) => {
+              errorHandling(error, data);
+            }
+          );
+        }
+      });
+
+      //finally add the transaction with transaction id in transaction schema
+      let transaction = new Transaction();
+      transaction.transaction_id = "11111";
+      transaction.account_id = account_id;
+      transaction.amount = amount;
+
+      transaction.save((error, data) => {
+        errorHandling(error, data);
+      });
+
+      res.status(200).json({
+        success: true,
+        message: "fav added successfully",
+        data: result,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+router.get("/transactions", (req, res) => {
+  Transaction.find((error, data) => {
+    errorHandling(error, data);
+    res.status(200).json({
+      success: true,
+      message: "user orders",
+      data: data,
+    });
+  }).sort({ created_at: -1 });
+});
 
 router.get("/accounts/:account_id", (req, res) => {
   try {
     Account.findOne({ account_id: req.params.account_id }, (error, data) => {
-      if (error) {
-        return res.status(400).json({
-          success: false,
-          message: "Invalid data.",
-        });
-      }
+      errorHandling(error, data);
+
       res.status(200).json({
         success: true,
         data: data,
@@ -25,5 +99,15 @@ router.get("/accounts/:account_id", (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 });
+
+errorHandling = async (error, data) => {
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid data.",
+    });
+  }
+  return data;
+};
 
 module.exports = router;
--- app-express/src/models/transaction-request.js
+++ app-express/src/models/transaction-request.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 // Define collection and schema
 let TransactionRequest = new Schema(
   {
